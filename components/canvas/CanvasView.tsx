21caa5ef2769d57c612ed69560c44b19e4bc2dc7
ZYan08 <zyyan.l08@gmail.com>
2025-08-17 00:10:37 +1000

    fix error

--- components/canvas/CanvasView.tsx
+++ components/canvas/CanvasView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import ReactFlow, {
   type Node,
   type Edge,
